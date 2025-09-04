7deddbf9a84c8e5d89d259c466cce16681b12558
Sanjayapotti <sanju.potti222@gmail.com>
2025-09-04 09:33:16 +0530

    Add files via upload

---
+++ trycatch.js
@@ -0,0 +1,13 @@
+let x=5,z=10,a=10,b=20;
+try { 
+    let y = x + z;
+ console.log("y:",y);
+let c=a*b;
+console.log("c:",c);
+let d=a/b;
+console.log("d:",d);
+
+}
+ catch (error) {   
+    console.error("An error occurred:", error.message);
+}
